287467380b0f8a6a12ecd61107b7a238a8ee232d
tusharshaha <tusharshaha128@gmail.com>
2024-12-23 21:02:42 +0600

    Init: created boilarplate for project

---
+++ src/app/layout.js
@@ -0,0 +1,20 @@
+import { Inter } from 'next/font/google';
+import './globals.css';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata = {
+  title: 'Skill360 - Learn Anytime, Anywhere',
+  description:
+    'Skill360 is your ultimate platform for online learning in Bangla. Explore courses, develop new skills, and achieve your goals with ease.',
+  keywords:
+    'e-learning, online courses, freelancing, Skill360, education, skills development'
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
