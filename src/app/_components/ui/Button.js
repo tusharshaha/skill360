c675f23701069d152634198b06751c59ebd4c501
tusharshaha <tusharshaha128@gmail.com>
2024-12-23 21:07:52 +0600

    UI: created button component

---
+++ src/app/_components/ui/Button.js
@@ -0,0 +1,14 @@
+'use client';
+
+import React from 'react';
+
+
+const Button = ({ children, className, onClick }) => {
+  return (
+    <button onClick={onClick} className={className}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;
