4af7d4bb851660d55fca47b170434a1a9b0edfef
tusharshaha <tusharshaha128@gmail.com>
2024-12-26 11:48:27 +0600

    add: added sub menu on navlink

--- frontend/src/app/_components/ui/Sidebar.jsx
+++ frontend/src/app/_components/ui/Sidebar.jsx
@@ -3,7 +3,8 @@ import { FaAngleDown } from 'react-icons/fa6';
 import { MdSchool } from 'react-icons/md';
 
 const Sidebar = ({ menu, className }) => {
-  const [open, setOpen] = useState();
+  const [open, setOpen] = useState(null);
+
   return (
     <div
       className={`trans h-screen w-[250px] absolute top-0 bg-white shadow-2xl ${className}`}
@@ -15,13 +16,18 @@ const Sidebar = ({ menu, className }) => {
       <ul className="">
         {menu.map((ele, i) => (
           <li key={i} className="border-b px-6 py-2">
-            <button className="flex items-center justify-between text-lg w-full p-2">
+            <button onClick={()=> setOpen(i)} className="flex items-center justify-between text-lg w-full p-2">
               <div className="flex items-center gap-2">
                 <MdSchool />
                 <span>{ele.name}</span>
               </div>
               <FaAngleDown />
             </button>
+            <ul className={`${open === i ? "block": "hidden"} bg-indigo-100`}>
+              {
+                ele.subLink.map((ele, i)=> <li key={i} className='py-1 hover:bg-slate-200 trans px-3'>{ele}</li>)
+              }
+            </ul>
           </li>
         ))}
       </ul>
